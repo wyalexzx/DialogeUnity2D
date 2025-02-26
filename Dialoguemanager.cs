32a5904f696921fa4fbd68faa9637763589fa611
wyalexzx <alejandromarquez6703@gmail.com>
2025-02-26 02:25:01 +0000

    Dialoguemanager.cs

--- Dialoguemanager.cs
+++ Dialoguemanager.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class DialogeManager : MonoBehaviour
 {
+
+    public Text nameText;
+    public Text DialogueText;
+
+
     private Queue<string> sentences;
     
     
@@ -14,10 +21,39 @@ public class DialogeManager : MonoBehaviour
         sentences = new Queue<string>();
     }
     
-    
+    public void StartDialogue (Dialoge dialogue)
     {
+        Debug.Log("starting conversation with " + dialogue.name);
 
-        
+        nameText.text = dialogue.name;
+
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentence.enqueue(sentence);
+        }
+
+        DisplayNextSetence();
+
     }
 
+    public void DisplayNextSentence ()
+    {
+        if (sentences.Count == 0)
+        {
+            endDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        DialogueText.text = sentence;
+
+    }
+
+    void EndDialogue()
+    {
+        Debug.Log("End of conversation. ");
+    }
 }
